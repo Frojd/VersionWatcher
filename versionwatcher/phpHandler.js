c1b2667ae6f684144ddaba7aa3056027d82abed6
Martin Sandström <martin@marteinn.se>
2017-02-23 15:58:50 +0100

    Renamed handler to phpHandler

--- versionwatcher/phpHandler.js
+++ versionwatcher/phpHandler.js
@@ -6,12 +6,14 @@ module.exports.trackPHP = (event, context, callback) => {
     // secret
     // data
     //
-    // const body = JSON.parse(event.input.body);
+    const result = JSON.parse(event.body)
+    // const result = {};
 
     const response = {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Go Serverless v1.0! Your function executed successfully!',
+            result: result,
             input: event,
         }),
     };
