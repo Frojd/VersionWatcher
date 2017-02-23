173ff9012db3f83a465086944ff8bc990a1cf044
Martin Sandström <martin@marteinn.se>
2017-02-23 22:16:04 +0100

    Renamed test handler

--- versionwatcher/trackerWP.js
+++ versionwatcher/trackerWP.js
@@ -5,13 +5,13 @@ function handler(event, context, callback) {
     const project = event.queryStringParameters.project;
     const release = event.queryStringParameters.release;
 
-    let pluginList = JSON.parse(event.body);
+    let packages = JSON.parse(event.body);
 
-    pluginList = pluginList.map((item) => {
+    packages = packages.map((item) => {
         return {name: item.name, version: item.version};
     });
 
-    pluginList.push({
+    packages = [].concat(packages, {
         name: 'wordpress',
         version: wpVersion,
     });
@@ -19,19 +19,25 @@ function handler(event, context, callback) {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            params: {
-                wpVersion: wpVersion,
-                project: project,
-                release: release,
-            },
-            result: pluginList,
-            input: event,
         }),
     };
+
+    /*
+    trackRelease({
+        project: project,
+        release: release,
+        packages: packages,
+        success: () => {
+        },
+        error: (error) => {
+        }
+    });
+    */
 
     callback(null, response);
 };
 
 
-module.exports.handler = handler;
+module.exports = {
+    handler: handler,
+};
