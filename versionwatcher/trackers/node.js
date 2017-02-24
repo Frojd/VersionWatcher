12bedc2f99a64e1111df7e12eed4979e3e11cee7
Daniel Karlsson <daniel.karlsson@frojd.se>
2017-02-24 14:41:21 +0100

    Added postversion of nodepackages

--- versionwatcher/trackers/node.js
+++ versionwatcher/trackers/node.js
@@ -4,53 +4,45 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 function handler(event, context, callback) {
-    // project
-    // stage
-    // secret
-    // data
-    //
-    // const body = JSON.parse(event.input.body);
+    const packageJson = JSON.parse(event.body);
 
+    let packages = [];
+    for(let key in packageJson.dependencies) {
+        packages.push({
+            "name": key,
+            "version": packageJson.dependencies[key]
+        });
+    }
+
+    for(let key in packageJson.devDependencies) {
+        packages.push({
+            "name": key,
+            "version": packageJson.devDependencies[key]
+        });
+    }
 
     let params = {
         TableName: 'VersionWatcher',
         Item: {
-            'project': 'Frojd/HRF-HRF.se',
-            'version': '4.5.0',
-            'type': 'WordPress',
-            'languages': 'php',
-            'packages': [
-                {
-                    "react": "2.6.0"
-                },
-                {
-                    "webpack": "2.3.0"
-                }
-            ]
+            'project': packageJson.name,
+            'version': packageJson.version,
+            'label': 'node',
+            'languages': 'javascript',
+            'packages': packages
         }
     };
 
     docClient.put(params, function(err, data) {
-        if (err) {
-            console.log('Unable to add item. Error JSON: ', JSON.stringify(err, null, 2));
-        } else {
-            console.log('Added');
-        }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                err: err,
+                data: data,
+            }),
+        };
+
+        callback(null, response);
     });
-
-
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0 NODE! Your function executed successfully!',
-            input: event,
-        }),
-    };
-
-    callback(null, response);
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
 module.exports = {
