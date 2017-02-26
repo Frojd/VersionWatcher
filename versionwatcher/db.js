d22e8f0a2a0e5482f4f04d1ca79cbb813a16cc0f
Martin Sandström <martin@marteinn.se>
2017-02-26 22:26:06 +0100

    Created document client helper

---
+++ versionwatcher/db.js
@@ -0,0 +1,14 @@
+const settings = require('./settings');
+
+function getDocumentClient() {
+    if (settings.CUSTOM_DOCUMENT_CLIENT) {
+        return settings.CUSTOM_DOCUMENT_CLIENT;
+    }
+
+    const AWS = require('aws-sdk');
+    return new AWS.DynamoDB.DocumentClient();
+}
+
+module.exports = {
+    getDocumentClient: getDocumentClient,
+}
