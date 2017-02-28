068e6c102b1276a3ea755127507cabab2616a436
Martin Sandström <martin@marteinn.se>
2017-02-28 21:54:36 +0100

    Refactored the node handler to use new db inserter

--- versionwatcher/trackers/node.js
+++ versionwatcher/trackers/node.js
@@ -1,44 +1,44 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient();
+const track = require('../helpers').track;
 
 function handler(event, context, callback) {
     const packageJson = JSON.parse(event.body);
     const project = event.queryStringParameters.project;
 
     let packages = [];
-    for(let key in packageJson.dependencies) {
-        packages.push({
-            "name": key,
-            "version": packageJson.dependencies[key]
-        });
-    }
+    let devPackages = [];
 
-    for(let key in packageJson.devDependencies) {
-        packages.push({
-            "name": key,
-            "version": packageJson.devDependencies[key]
-        });
-    }
-
-    let params = {
-        TableName: 'VersionWatcher',
-        Item: {
-            'project': project,
-            'version': packageJson.version,
-            'label': 'node',
-            'languages': 'javascript',
-            'packages': packages
+    packages = Object.keys(packageJson.dependencies).map((key) => {
+        return {
+            name: key,
+            version: packageJson.dependencies[key]
         }
-    };
+    });
 
-    docClient.put(params, function(err, data) {
+    devPackages = Object.keys(packageJson.devDependencies).map((key) => {
+        return {
+            name: key,
+            version: packageJson.dependencies[key]
+        }
+    });
+
+    packages = packages.concat(devPackages);
+
+    track({
+        project: project,
+        version: packageJson.version,
+        packages: packages,
+        label: 'node',
+        languages: 'javascript',
+    }, (err, model) => {
         const response = {
             statusCode: 200,
             body: JSON.stringify({
-                err: err,
-                data: data,
+                input: event,
+                version: packageJson.version,
+                project: project,
+                packages: packages,
             }),
         };
 
