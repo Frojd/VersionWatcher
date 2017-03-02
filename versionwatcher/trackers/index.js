a39d31afd7b8f06279cd4b2e6ba9d1229a26a0fa
Martin Sandström <martin@marteinn.se>
2017-03-02 09:24:26 +0100

    Fixed issue with python namespace

--- versionwatcher/trackers/index.js
+++ versionwatcher/trackers/index.js
@@ -1,6 +1,6 @@
 const node = require('./node').handler;
 const wp = require('./wp').handler;
-const python = require('./wp').handler;
+const python = require('./python').handler;
 
 module.exports = {
     node,
