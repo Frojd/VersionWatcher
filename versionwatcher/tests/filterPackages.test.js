843a1c6a6b4c1a7198e737d59caba475f9764840
Martin Sandström <martin@marteinn.se>
2017-03-23 14:00:59 +0100

    Updated test data

--- versionwatcher/tests/filterPackages.test.js
+++ versionwatcher/tests/filterPackages.test.js
@@ -9,7 +9,25 @@ describe('Test result endpoints', () => {
         let items;
         const versions = [
             {
-                packages: [
+                "version": "v1.1.1",
+                "languages": "php",
+                "branch": "master",
+                "label": "wordpress",
+                "created": 1490269584,
+                "project": "Frojd/Client-Project2",
+                "packages": [
+                    {
+                        "name": "dynamic-hostname",
+                        "version": "0.4.2"
+                    },
+                    {
+                        "name": "tinymce-templates",
+                        "version": "4.4.3"
+                    },
+                    {
+                        "name": "wp-multibyte-patch",
+                        "version": "2.4"
+                    },
                     {
                         "name": "wp-total-hacks",
                         "version": "2.0.1"
@@ -18,8 +36,9 @@ describe('Test result endpoints', () => {
                         "name": "wordpress",
                         "version": "4.7.0"
                     }
-                ]
-            }
+                ],
+                "commit": "randomrandomrandom"
+            },
         ];
 
         items = filterVersionsByPackage(versions);
