70c97394b10a9b1f2aa21c85914a278c7f4775c9
Martin Sandström <martin@marteinn.se>
2017-04-27 14:42:44 +0200

    Created separate stage config

--- versionwatcher/settings.js
+++ versionwatcher/settings.js
@@ -1,3 +1,6 @@
 module.exports = {
     OVERRIDE_DOCUMENT_CLIENT: null,
+    TABLE_PACKAGE: process.env.TABLE_PACKAGE || 'VersionWatcherPackage',
+    TABLE_VERSION: process.env.TABLE_VERSION || 'VersionWatcherVersion,
+    TABLE_STABLE: process.env.TABLE_STAGE || 'VersionWatcherStable'
 }
