74e38483cb57fa2d97b2a75a315733077cb86347
Martin Sandström <martin@marteinn.se>
2017-04-27 11:05:31 +0200

    Added tracking for node

--- tools/track.sh
+++ tools/track.sh
@@ -6,23 +6,28 @@
 CMD=$1
 SERVICE_DOMAIN=https://ei5v6h5fz6.execute-api.eu-west-1.amazonaws.com
 
-case "$CMD" in
-    "wordpress-classic" )
-        ;;
+PROJECT="$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
+VERSION=${CIRCLE_TAG:-$CIRCLE_SHA1}
 
+# TODO: Add check to make sure TRACKER_API_KEY exist
+# TODO: Make it possible to override service domain
+
+case "$CMD" in
     "wordpress" )
+        WP_VERSION=$(./wp-cli.phar core version)
+        URL="$SERVICE_DOMAIN/stage/tracker/wp?project=$PROJECT&version=$VERSION&wpversion=$WP_VERSION&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
+        curl -X POST $URL -H "Content-Type: application/json; charset=utf-8" -H "x-api-key: $TRACKER_API_KEY" -d $(./wp-cli.phar plugin list --format=json)
         ;;
 
     "python" )
         LABEL=${2:-django}
-        PROJECT="$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
-        VERSION=${CIRCLE_TAG:-$CIRCLE_SHA1}
         pip freeze > post-requirements.txt
         URL="$SERVICE_DOMAIN/stage/tracker/python?project=$PROJECT&version=$VERSION&label=$LABEL&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
-
         curl -X POST $URL -H "Content-Type: text/plain; charset=utf-8" -H "x-api-key: $TRACKER_API_KEY" --data-binary @post-requirements.txt
         ;;
 
-    "nodejs" )
+    "node" )
+        URL="$SERVICE_DOMAIN/stage/tracker/node?project=$PROJECT&version=$VERSION&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
+        curl -X POST $URL -H "Content-Type: application/json; charset=utf-8" -H "x-api-key: $TRACKER_API_KEY" -d @package.json
         ;;
 esac
