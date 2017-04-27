8d01d80c7bb788433fbbac7038b984f39e1cae05
Martin Sandström <martin@marteinn.se>
2017-04-27 16:37:09 +0200

    Updated build tool base config

--- tools/track.sh
+++ tools/track.sh
@@ -3,7 +3,7 @@
 
 CMD=$1
 
-SERVICE_DOMAIN=${TRACKER_API:=https://ei5v6h5fz6.execute-api.eu-west-1.amazonaws.com}
+SERVICE_DOMAIN=${TRACKER_API:=https://n2t2izj4a0.execute-api.eu-west-1.amazonaws.com}
 PROJECT="$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 VERSION=${CIRCLE_TAG:-$CIRCLE_SHA1}
 
@@ -12,19 +12,19 @@ if [ -z "$TRACKER_API_KEY" ]; then echo "Error: Missing TRACKER_API_KEY value"; 
 case "$CMD" in
     "wordpress" )
         WP_VERSION=$(./wp-cli.phar core version)
-        URL="$SERVICE_DOMAIN/stage/tracker/wp?project=$PROJECT&version=$VERSION&wpversion=$WP_VERSION&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
+        URL="$SERVICE_DOMAIN/prod/tracker/wp?project=$PROJECT&version=$VERSION&wpversion=$WP_VERSION&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
         curl -X POST $URL -H "Content-Type: application/json; charset=utf-8" -H "x-api-key: $TRACKER_API_KEY" -d $(./wp-cli.phar plugin list --format=json)
         ;;
 
     "python" )
         LABEL=${2:-django}
         pip freeze > post-requirements.txt
-        URL="$SERVICE_DOMAIN/stage/tracker/python?project=$PROJECT&version=$VERSION&label=$LABEL&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
+        URL="$SERVICE_DOMAIN/prod/tracker/python?project=$PROJECT&version=$VERSION&label=$LABEL&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
         curl -X POST $URL -H "Content-Type: text/plain; charset=utf-8" -H "x-api-key: $TRACKER_API_KEY" --data-binary @post-requirements.txt
         ;;
 
     "node" )
-        URL="$SERVICE_DOMAIN/stage/tracker/node?project=$PROJECT&version=$VERSION&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
+        URL="$SERVICE_DOMAIN/prod/tracker/node?project=$PROJECT&version=$VERSION&branch=$CIRCLE_BRANCH&commit=$CIRCLE_SHA1"
         curl -X POST $URL -H "Content-Type: application/json; charset=utf-8" -H "x-api-key: $TRACKER_API_KEY" -d @package.json
         ;;
 esac
