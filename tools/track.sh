508d5277dd58b2a073e001e70d7f79a73d771b89
Martin Sandström <martin@marteinn.se>
2017-04-28 10:22:40 +0200

    Added command for bedrock setup

--- tools/track.sh
+++ tools/track.sh
@@ -11,13 +11,15 @@ if [ -z "$TRACKER_API_KEY" ]; then echo "Error: Missing TRACKER_API_KEY value"; 
 
 case "$CMD" in
     "wp-bedrock-circle-setup" )
-        echo "Install bedrock"
+        curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x ./wp-cli.phar
+        mv test.env .env
+        ./wp-cli.phar core install --allow-root --admin_name=admin --admin_password=admin --admin_email=admin@example.com --url=http://example.com.dev --title=WordPress
         ;;
 
     "wp-circle-setup" )
         curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x ./wp-cli.phar
         ./wp-cli.phar core config --allow-root --dbname=circle_test --dbuser=ubuntu --dbhost=127.0.0.1
-        ./wp-cli.phar core install --allow-root --admin_name=admin --admin_password=admin --admin_email=admin@example.com --url=http://exmaple.com.dev --title=WordPress
+        ./wp-cli.phar core install --allow-root --admin_name=admin --admin_password=admin --admin_email=admin@example.com --url=http://example.com.dev --title=WordPress
         ;;
 
     "wordpress" )
