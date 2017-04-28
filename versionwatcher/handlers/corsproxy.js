f46a8559d2e8d6dbc1edf87883163faf6ad0e55f
Daniel Karlsson <daniel.karlsson@frojd.se>
2017-04-28 16:05:55 +0200

    Added corstest and changed to plain javascript get

--- versionwatcher/handlers/corsproxy.js
+++ versionwatcher/handlers/corsproxy.js
@@ -1,29 +1,25 @@
 'use strict';
 
-var fetch = require('node-fetch');
+var http = require('https');
 
 function corsHandler(event, context, callback) {
     let queryStringParameters = event.queryStringParameters || {};
     let corsUrl = queryStringParameters.corsUrl;
 
-    fetch(corsUrl, {
-        method: 'get'
-    }).then(function(response) {
-        return response.text();
-    }).then(function(text) {
-        const response = {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET,OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
-            },
-            body: text
-        };
-
-        callback(null, response);
-    }).catch(function(err) {
-        callback(err);
+    http.get(corsUrl, (res) => {
+        res.setEncoding('utf8');
+        res.on('data', function (body) {
+            const response = {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET,OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
+                },
+                body: body
+            };
+            callback(null, response)
+        });
     });
 }
 
